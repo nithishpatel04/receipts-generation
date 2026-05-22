ff102b48b75266ef0ecb35168a7773094d70dc06
nithishpatel04 <patelnithish@gmail.com>
2026-05-22 00:05:34 -0400

    Updated

--- app.py
+++ app.py
@@ -131,8 +131,9 @@ def admin():
     return render_template("admin.html", orders=[o.to_dict() for o in orders])
 
 
+# Create database tables
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    # Ensure DB tables are created within the application context
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
